a2a8faeaa4279efc9aeedffeec85f032b6143f04
Mohd Shadab <101493917+Mshadab123@users.noreply.github.com>
2022-03-30 14:08:19 +0530

    Add files via upload

---
+++ MiniProject_c/M1_Project_Cricket_Score_Sheet/Cricket Scoresheet/menu.h
@@ -0,0 +1,10 @@
+//learnprogramo - programming made simple
+void menu()
+{
+    printf("\n\n\t\t\t\t\tMENU:\n");
+    printf("\n\n\n\t\t\t\t1.New scoresheet:\n");
+    printf("\t\t\t\t2.View scoresheet:\n");
+    printf("\t\t\t\t3.Exit:\n\t\t");
+}
+
+
